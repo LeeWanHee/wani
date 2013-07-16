148f992c442b7a91b2d4cdc6422b03111bb046ca
Wani <wani@ubuntu.ubuntu-domain>
2013-07-16 13:11:45 +0900

    update

--- git-dir/kernel_mo/hello.c
+++ git-dir/kernel_mo/hello.c
@@ -3,8 +3,17 @@
 #include<linux/module.h>
 #include<linux/miscdevice.h>
 
+static struct miscdevice hello_dev;
+
+int hello_open(void)
+{
+	printk(KERN_ALERT "open pen\n");
+	return 0;
+}
+
 int __init init_hello(void)
 {
+	misc_register(&hello_dev);
 	printk(KERN_ALERT "[Module Message] Hello, Module.\n");
 	return 0;
 }
@@ -13,14 +22,13 @@ void __exit exit_hello(void)
 {
 	printk(KERN_ALERT "[Module Message] Do you really wnat to break up with me?\n");
 }
-static struct miscdevice char_dev = 
-{
-	.minor = MISC_DYNAMIC_MINOR,
-	.name = "preempt_test"
-	.fops = &char_fops
+static struct file_operations hello_fops={
+.open = hello_open,
 };
 
-misc_register(&char_dev);
+static struct miscdevice hello_dev={
+	253, "hello_misc",&hello_fops };
+
 module_init(init_hello);
 module_exit(exit_hello);
 MODULE_LICENSE("GPL");
